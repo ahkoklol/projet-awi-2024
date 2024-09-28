85e1e88551eddff4ab0ea4fb6af601973cc64b84
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2024-09-28 19:52:54 +0200

    login and signout

---
+++ src/components/auth.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "../App.css";
+import { auth, googleProvider } from "../config/firebase";
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+
+export const Auth = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // check the user
+    console.log(auth?.currentUser?.email)
+
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            console.log('User created');
+        } catch (error) {
+            console.log('User not created', error);
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+            console.log('User signed in with Google');
+        } catch (error) {
+            console.log('User not signed in with Google', error);
+        }
+    };
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
+            console.log('User signed out');
+        } catch (error) {
+            console.log('User not signed out', error);
+        }
+    }
+
+    return (
+        <div>
+            <input placeholder="Email..." onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="Password..." type="password" onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={signIn}>Sign In</button>
+            <button onClick={signInWithGoogle}>Sign In with Google</button>
+            <button onClick={logout}>Log Out</button>
+        </div>
+    );
+};
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase configuration with environment variables
 const firebaseConfig = {
@@ -15,3 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
